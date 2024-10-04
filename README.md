440961e5b09cd9e2bb3200f63607887ed590867d
Andromeda-NGC224 <shadim115115@gmail.com>
2024-10-04 14:08:17 +0300

    init project

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Next
+# car-dealer
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-import AddCompanyButton from './components/addCompanyButton';
-import StatusLabel, { Status } from './components/statusLabel';
-
 export default function Home() {
   return (
     <main>
       <h1 className="text-xl">Home Page</h1>
-      <AddCompanyButton />
     </main>
   );
 }
